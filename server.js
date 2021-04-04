0a00ef131c44961d5ebbc64f3416cb00ee99c923
Omar-zoubi <o.fandi02@gmail.com>
2021-04-04 19:02:24 +0300

    zoubi

--- server.js
+++ server.js
@@ -1,44 +1,48 @@
 'use strict';
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3030;
 
 const express = require('express');
 const superagent = require('superagent');
 const path = require('path');
 
 const app = express();
-// app.use(superagent);
-
+app.use(errorHandler);
 app.use(express.static('public/styles'));
 app.use(express.urlencoded({
-    extended: true
+  extended: true
 }));
 
 app.set('view engine', 'ejs');
-// app.set('views', path.join(__filename, 'veiws'));
+app.set('views', path.join(__filename, 'veiws'));
 
 app.get('/searches/new', (req, res) => {
-    console.log('inside of searches!!');
-
-    res.render('pages/searches/new');
+  res.render('pages/searches/new');
 });
+function errorHandler(err, req, res, next) {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500);
+  res.render('pages/error', { error: err });
+}
 
 app.post('/searches', getData);
 
 function Book(data) {
-    this.title = data.volumeInfo.title;
-    this.author = data.volumeInfo.authors.join(', ');
-    this.description = data.volumeInfo.description;
-    this.image = (data.volumeInfo.imageLinks) ? data.volumeInfo.imageLinks.smallThumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
+  this.title = data.volumeInfo.title;
+  this.author = data.volumeInfo.authors.join(', ');
+  this.description = data.volumeInfo.description;
+  this.image = (data.volumeInfo.imageLinks) ? data.volumeInfo.imageLinks.smallThumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
 }
 
 function getData(req, res) {
 
-    let url = 'https://www.googleapis.com/books/v1/volumes?q=';
-    if (req.body['search-by'] === 'title') { url += `+intitle:${req.body['search-by']}`; }
-    if (req.body['search-by'] === 'author') { url += `+inauthor:${req.body['search-by']}`; }
-    superagent.get(url).then(data => {
-        return data.body.items.map(element => new Book(element))
-    })
+  let url = 'https://www.googleapis.com/books/v1/volumes?q=';
+  if (req.body['search-by'] === 'title') { url += `+intitle:${req.body['name']}`; }
+  if (req.body['search-by'] === 'author') { url += `+inauthor:${req.body['name']}`; }
+  superagent.get(url).then(data => {
+    return data.body.items.map(element => new Book(element));
+  })
     .then(results => res.render('pages/searches/show', { searchResults: results }));
 
 }
@@ -46,11 +50,12 @@ function getData(req, res) {
 
 
 
+
 app.get('*', (req, res) => {
-    res.status(404).send('Page not found');
-    console.log('page not found');
+  res.status(404).send('Page not found');
+  console.log('page not found');
 });
 app.listen(PORT, () => {
-    console.log('Listening on ', PORT);
+  console.log('Listening on ', PORT);
 });
 
